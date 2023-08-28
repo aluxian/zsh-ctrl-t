bf7de3f23b169ab66ef1e1b32a4cdf431f279112
Alexandru Rosianu <me@aluxian.com>
2023-08-28 10:37:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ctrl-t
+
+Press `ctrl-t` to quickly run a task from `package.json` or `Makefile`.
+
+## Dependencies
+
+- `fzf`
---
+++ ctrl-t.plugin.zsh
@@ -0,0 +1,19 @@
+function ctrl-t-select() {
+  if ! command -v fzf > /dev/null 2>&1; then
+    echo "Error: fzf command not found."
+  fi
+  
+  if [[ -f "package.json" ]]; then
+    tasks=$(jq '.scripts|keys[]' -rc package.json)
+  fi
+
+	BUFFER=$(jq '.scripts|keys[]' -rc package.json | fzf --query "$LBUFFER")
+  BUFFER="npm run ${BUFFER}"
+	CURSOR=$#BUFFER
+	
+  zle redisplay
+}
+
+autoload -U ctrl-t-select
+zle -N ctrl-t-select
+bindkey '^t' ctrl-t-select
